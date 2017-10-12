f01e2a76cfc54e899319a94da032a748a2323cc4
onekiloparsec <cedric@onekilopars.ec>
2017-10-12 11:08:45 +0200

    Making dev server work again
    Signed-off-by: onekiloparsec <cedric@onekilopars.ec>

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,9 +1,9 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var autoprefixer = require('autoprefixer');
-var fs = require('fs');
-var csswring = require('csswring');
+var path = require('path')
+var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var autoprefixer = require('autoprefixer')
+var fs = require('fs')
+var csswring = require('csswring')
 
 
 module.exports = {
@@ -19,8 +19,8 @@ module.exports = {
 		publicPath: '/dist/',
 	},
 
-	quiet: false,
-	noInfo: false,
+	// quiet: false,
+	// noInfo: false,
 	stats: {
 		// Config for minimal console.log mess.
 		assets: false,
@@ -37,7 +37,6 @@ module.exports = {
 			__DEVELOPMENT__: true
 		}),
 		new ExtractTextPlugin('bundle.css'),
-		new webpack.optimize.OccurenceOrderPlugin(),
 		new webpack.HotModuleReplacementPlugin(),
 		new webpack.NoErrorsPlugin(),
 		new webpack.ProvidePlugin({
@@ -46,8 +45,8 @@ module.exports = {
 	],
 
 	resolve: {
-		extensions: ['', '.js', '.json'],
-		modulesDirectories: ['node_modules', 'src'],
+		extensions: ['.js', '.json'],
+		modules: ['node_modules', 'src'],
 		alias: {
 			// require('tinymce') will do require('tinymce/tinymce') 
 			tinymce: 'tinymce/tinymce',
@@ -55,54 +54,64 @@ module.exports = {
 	},
 
 	module: {
-		loaders: [
-		{
-			test: /\.js$/,
-			loaders: ['babel-loader', 'eslint-loader'],
-			exclude: /node_modules/,
-		},
-		// {
-		// 	// Only apply on tinymce/tinymce
-		// 	include: require.resolve('tinymce/tinymce'),
-		// 	// Export window.tinymce
-		// 	loader: 'exports?window.tinymce',
-	 //    },
-		{
-        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
-        loader: 'url-loader'
-    },
-		// {
-		// 	test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-		// 	loader: 'url?limit=10000&mimetype=application/font-woff',
-		// }, {
-		// 	test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-		// 	loader: 'url?limit=10000&mimetype=application/font-woff2',
-		// }, {
-		// 	test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-		// 	loader: 'url?limit=10000&mimetype=application/octet-stream',
-		// }, {
-		// 	test: /\.otf(\?v=\d+\.\d+\.\d+)?$/,
-		// 	loader: 'url?limit=10000&mimetype=application/font-otf',
-		// }, {
-		// 	test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-		// 	loader: 'file',
-		// }, {
-		// 	test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-		// 	loader: 'url?limit=10000&mimetype=image/svg+xml',
-		// },  
-		{
-			test: /\.scss$/,
-			loader: 'css?localIdentName=[path]!postcss-loader!sass',
-		}, {
-			test: /\.png$/,
-			loader: 'file?name=[name].[ext]',
-		}, {
-			test: /\.jpg$/,
-			loader: 'file?name=[name].[ext]',
-		}
+		rules: [
+			{
+				test: /\.js$/,
+				loaders: ['babel-loader', 'eslint-loader'],
+				exclude: /node_modules/,
+			},
+			// {
+			// 	// Only apply on tinymce/tinymce
+			// 	include: require.resolve('tinymce/tinymce'),
+			// 	// Export window.tinymce
+			// 	loader: 'exports?window.tinymce',
+			//    },
+			{
+				test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+				loader: 'url-loader'
+			},
+			// {
+			// 	test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+			// 	loader: 'url?limit=10000&mimetype=application/font-woff',
+			// }, {
+			// 	test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+			// 	loader: 'url?limit=10000&mimetype=application/font-woff2',
+			// }, {
+			// 	test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+			// 	loader: 'url?limit=10000&mimetype=application/octet-stream',
+			// }, {
+			// 	test: /\.otf(\?v=\d+\.\d+\.\d+)?$/,
+			// 	loader: 'url?limit=10000&mimetype=application/font-otf',
+			// }, {
+			// 	test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+			// 	loader: 'file',
+			// }, {
+			// 	test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+			// 	loader: 'url?limit=10000&mimetype=image/svg+xml',
+			// },
+			{
+				test: /\.(sass|less|styl|sss|css)$/,
+				use: [
+					{
+						loader: 'css?localIdentName=[path]!postcss-loader!sass',
+						options: {
+							options: {/* PostCSS Options */},
+							plugins: () => [
+								autoprefixer({browsers: ['last 2 versions', 'safari 5', 'ie 9', 'ios 6', 'android 4']}),
+								csswring
+							]
+						}
+					}
+				]
+			},
+			{
+				test: /\.png$/,
+				loader: 'file?name=[name].[ext]',
+			},
+			{
+				test: /\.jpg$/,
+				loader: 'file?name=[name].[ext]',
+			}
 		],
-	},
-	postcss: function() {
-		return [autoprefixer({ browsers: ['last 2 versions', 'safari 5', 'ie 9', 'ios 6', 'android 4'] }), csswring];
-	},
-};
+	}
+}
